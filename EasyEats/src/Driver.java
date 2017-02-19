829408aaa2cee2bac4b641b8f9964cae74a54798
Elizabeth Caronia <Elizabeth Caronia>
2017-02-18 20:29:50 -0500

    easy eats contains db

---
+++ EasyEats/src/Driver.java
@@ -0,0 +1,28 @@
+/**
+ * Created by Elizabeth on 2/18/2017.
+ */
+import java.sql.*;
+public class Driver {
+
+    public static void main(String[] args){
+        try{
+            //1. get connection
+            String driver = "com.mysql.jdbc.Driver";
+            Class.forName(driver).newInstance();
+            Connection myConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/easyeats","root", "root");
+
+            //2. create statement
+            Statement myStat = myConn.createStatement();
+            //3. execue sql query
+            ResultSet myRa = myStat.executeQuery("select * from easyeats.kitchen_goods");
+            //4. process result set
+            while(myRa.next()){
+                System.out.println(myRa.getString("item")+" ");
+            }
+        }
+        catch (Exception exc){
+            exc.printStackTrace();
+        }
+    }
+}
+
