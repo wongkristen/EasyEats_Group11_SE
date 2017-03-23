18a7aab4373dcb08bd10d5ca96bc9b62f0a617f2
elizabeth <elizabeth.caronia@rutgers.edu>
2017-03-23 02:39:51 -0400

    eclipse dynamic web app

---
+++ RestaurantAutomation/WebContent/controll.js
@@ -0,0 +1,10 @@
+/**
+ *
+ */
+angular.module('myApp', []).controller('namesCtrl', function($scope) {
+    $scope.names = [
+        {name:'Jani',country:'Norway'},
+        {name:'Hege',country:'Sweden'},
+        {name:'Kai',country:'Denmark'}
+    ];
+});
