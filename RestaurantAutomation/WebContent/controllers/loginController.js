0a0ab7c4d804c3634818e6873233f1aff9c17b3b
elizabeth <elizabeth.caronia@rutgers.edu>
2017-03-26 01:17:22 -0400

    tabs work, calender next to do

--- RestaurantAutomation/WebContent/controllers/loginController.js
+++ RestaurantAutomation/WebContent/controllers/loginController.js
@@ -5,39 +5,39 @@ app.controller("LoginController", function($scope, $http, $state){
 			username:undefined,
 			password:undefined
 	}
-	
+
 	$scope.loginInfo = {
 			username:undefined,
 			password:undefined
 	}
-	
+
 	//Functions
-	
+
 	$scope.signUserUp = function() {
 		var data = {
 				username: $scope.signUpInfo.username,
 				password: $scope.signUpInfo.password
 		}
-		
+
 		$http.post("endpoints/signup.php", data).success(function(response) {
 			console.log(response);
 			localStorage.setItem("user", JSON.stringify({user: response}))
-			$state.go("manager");
+			$state.go("managerHome");
 		}).error(function(error) {
 			console.error(error);
 		})
 	}
-	
+
 	$scope.loginUser = function() {
 		var data = {
 				username: $scope.loginInfo.username,
 				password: $scope.loginInfo.password
 		}
-		
+
 		$http.post("endpoints/login.php", data).success(function(response) {
 			console.log(response);
 			localStorage.setItem("user", JSON.stringify({user: response[0].email}));
-			$state.go("manager");
+			$state.go("managerHome");
 		}).error(function(error) {
 			console.error(error);
 		})
